15afa990f6ecad516bf3f6f5f2a9a4852e6c68a6
felipetexa <felipe.teixeira91@hotmail.com>
2022-06-26 15:49:11 -0300

    aula 03 - implementando video e roteamento

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ignite-react
+
+Acompanhamento da semana Ignite Labs da Rocketseat, para aprender a desenvolver uma aplicação em ReactJS
+
+A ideia é criar um clone da plataforma Ignite da Rocketseat.
+
+Motivo pessoal: primeiros contatos com a lib React, a fim de aprender seus conceitos de maneira prática, para depois utilizar no mercado de trabalho
--- event-platform/src/App.tsx
+++ event-platform/src/App.tsx
@@ -1,11 +1,19 @@
+import { Router } from "./Router"
 import { Event } from "./pages/Event"
+import { ApolloProvider } from "@apollo/client"
+import { client } from "./lib/apollo"
+import { BrowserRouter } from "react-router-dom"
 
 
 function App() {
 
   return (
     <div>
-      <Event />
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </ApolloProvider>
     </div>
   )
 }
